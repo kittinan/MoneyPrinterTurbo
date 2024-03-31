d47f988d105c5deb30bf8c5e2ce8989183ccea0a
Kittinan <kittinan@satisbuy.com>
2024-03-31 22:47:23 +0700

    add font

--- webui/Main.py
+++ webui/Main.py
@@ -375,13 +375,13 @@ with right_panel:
         with font_cols[0]:
             params.text_fore_color = st.color_picker(tr("Font Color"), "#FF0000")
         with font_cols[1]:
-            params.font_size = st.slider(tr("Font Size"), 30, 100, 60)
+            params.font_size = st.slider(tr("Font Size"), 30, 180, 60, 2)
 
         stroke_cols = st.columns([0.3, 0.7])
         with stroke_cols[0]:
             params.stroke_color = st.color_picker(tr("Stroke Color"), "#FF0000")
         with stroke_cols[1]:
-            params.stroke_width = st.slider(tr("Stroke Width"), 0.0, 10.0, 1.5)
+            params.stroke_width = st.slider(tr("Stroke Width"), 0.0, 10.0, 1.5, 0.25)
 
 start_button = st.button(tr("Generate Video"), use_container_width=True, type="primary")
 if start_button:
